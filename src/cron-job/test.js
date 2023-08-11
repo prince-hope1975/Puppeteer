aaeb3ac41cf618eb01cf521a1a31aa35cce12180
Prince <amachreeprince7@gmail.com>
2023-08-11 11:54:09 +0100

    damn

--- src/cron-job/test.js
+++ src/cron-job/test.js
@@ -1,6 +1,6 @@
 // @ts-check
 import puppeteer from "puppeteer";
-import { getFloor } from "../puppeteer";
+import { getFloor } from "../puppeteer/index.js";
 
 const browser = await puppeteer.launch();
 const floor = await getFloor("algoatspfp", browser);
