eff8c44add1343fcddd48724c5cee000a8de9eb9
Prince <amachreeprince7@gmail.com>
2023-08-11 16:10:57 +0100

    ejor

--- src/cron-job/test.js
+++ src/cron-job/test.js
@@ -3,10 +3,10 @@ import puppeteer from "puppeteer";
 import { getFloor } from "../puppeteer/index.js";
 
 const browser = await puppeteer.launch({
-  headless: true,
+  headless: "new",
   //   headless: true,
   //   executablePath: "/usr/bin/chromium-browser",
 });
-const floor = await getFloor("algoatspfp", browser);
+const floor = await getFloor("algogods", browser);
 console.log({ floor });
 process.exit();
