ecdc39265f8bbcc30c02cb52d4e6af14f6d2959c
Prince <amachreeprince7@gmail.com>
2023-08-11 13:22:03 +0100

    oluwa

--- src/cron-job/test.js
+++ src/cron-job/test.js
@@ -2,7 +2,7 @@
 import puppeteer from "puppeteer";
 import { getFloor } from "../puppeteer/index.js";
 
-const browser = await puppeteer.launch({ headless: "new" });
+const browser = await puppeteer.launch({ headless: false ,});
 const floor = await getFloor("algoatspfp", browser);
 console.log({ floor });
 process.exit()
