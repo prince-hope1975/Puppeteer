8faf40a0c226732f30a426515f5e142fd2b67372
Prince <amachreeprince7@gmail.com>
2023-08-11 16:36:19 +0100

    no work

--- src/cron-job/test.js
+++ src/cron-job/test.js
@@ -2,7 +2,9 @@
 import puppeteer from "puppeteer";
 import { getFloor } from "../puppeteer/index.js";
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+    headless:"new"
+});
 console.log("Browser launched")
 const floor = await getFloor("algogods", browser);
 console.log({ floor });
