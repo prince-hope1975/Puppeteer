cd3b560512bd4ce9da84d025eadbffa0984daa16
Prince <amachreeprince7@gmail.com>
2023-08-19 18:43:47 +0100

    abeg

--- src/index.js
+++ src/index.js
@@ -31,10 +31,12 @@ app.get("/floor-price/:collection", async (req, res) => {
         const envVariables = {
             KEY: _collection, // Example variable
         };
+        const _path = path.resolve(`src/start.js`);
+        console.log({ _path });
         const envVariableArgs = Object.keys(envVariables)
             .map((key) => `-e ${key}=${envVariables["KEY"]}`)
             .join(" ");
-        const command = `docker run -i --init --cap-add=SYS_ADMIN --rm ${envVariableArgs} ghcr.io/puppeteer/puppeteer:latest node -e "$(cat ${path.resolve(`src/start.js`)})"`;
+        const command = `docker run -i --init --cap-add=SYS_ADMIN --rm ${envVariableArgs} ghcr.io/puppeteer/puppeteer:latest node -e "$(cat ${_path})"`;
         // !This works only on my local machine
         // Once it's on the sever it fails proably due to the way the directory is read
         // const command = `docker run -i --init --cap-add=SYS_ADMIN --rm ${envVariableArgs} ghcr.io/puppeteer/puppeteer:latest node -e "$(cat ./src/start.js)"`;
