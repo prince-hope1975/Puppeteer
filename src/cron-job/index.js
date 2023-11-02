21bcba06f75e41190a4428e32f0b3c6f8576cb48
Prince <amachreeprince7@gmail.com>
2023-11-02 20:17:39 +0100

    hmm

--- src/cron-job/index.js
+++ src/cron-job/index.js
@@ -12,7 +12,6 @@ export const Func = async () => {
         return;
     if (typeof _floor === "object") {
         for (const key in _floor) {
-            console.log({ key });
             let browser = await puppeteer
                 .launch({
                 headless: "new",
@@ -28,14 +27,11 @@ export const Func = async () => {
             // browser
             console.log({ key });
             try {
-                console.log("Fetching");
                 const floor = await getFloor_withBrowser(browser, key);
-                console.log("Fetched");
                 const floor_price = parseLocaleNumber(floor?.at(1), "en-US");
                 await FLOOR_REF.child(key).set(floor_price);
             }
             catch (error) {
-                console.log("Launching new browser");
                 const browser = await puppeteer.launch({
                     headless: "new",
                     timeout: 120_000,
@@ -46,16 +42,13 @@ export const Func = async () => {
                     await FLOOR_REF.child(key).set(floor_price);
                 }
                 catch (error) {
-                    console.log("continuing 1");
                     findAndKillLatestChromeProcess(browser.process()?.pid);
                     console.error(error);
                     continue;
                 }
                 findAndKillLatestChromeProcess(browser.process()?.pid);
-                console.log("continuing 1");
                 continue;
             }
-            console.log("continuing 2");
             findAndKillLatestChromeProcess(browser.process()?.pid);
         }
     }
--- src/cron-job/index.ts
+++ src/cron-job/index.ts
@@ -15,7 +15,6 @@ export const Func = async () => {
   if (!_floor) return;
   if (typeof _floor === "object") {
     for (const key in _floor) {
-      console.log({ key });
       let browser = await puppeteer
         .launch({
           headless: "new",
@@ -31,13 +30,10 @@ export const Func = async () => {
       // browser
       console.log({ key });
       try {
-        console.log("Fetching");
         const floor = await getFloor_withBrowser(browser, key);
-        console.log("Fetched");
         const floor_price = parseLocaleNumber(floor?.at(1), "en-US");
         await FLOOR_REF.child(key).set(floor_price);
       } catch (error) {
-        console.log("Launching new browser");
         const browser = await puppeteer.launch({
           headless: "new",
           timeout: 120_000,
@@ -47,16 +43,13 @@ export const Func = async () => {
           const floor_price = parseLocaleNumber(floor?.at(1), "en-US");
           await FLOOR_REF.child(key).set(floor_price);
         } catch (error) {
-          console.log("continuing 1");
           findAndKillLatestChromeProcess(browser.process()?.pid);
           console.error(error);
           continue;
         }
         findAndKillLatestChromeProcess(browser.process()?.pid);
-        console.log("continuing 1");
         continue;
       }
-      console.log("continuing 2");
       findAndKillLatestChromeProcess(browser.process()?.pid);
     }
   }
