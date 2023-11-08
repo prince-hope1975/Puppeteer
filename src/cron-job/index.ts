19612ce21fcfc278c0bbd0a310f454c27c37e6d2
Prince <amachreeprince7@gmail.com>
2023-11-08 04:41:48 +0100

    work

--- src/cron-job/index.ts
+++ src/cron-job/index.ts
@@ -52,7 +52,7 @@ export const Func = async () => {
 //   console.log("Done");
 // });
 
-schedule("* */1 * * *", () => {
+schedule("* */2 * * *", () => {
   Func()
     .then(() => {
       console.log({ res: "success" });
